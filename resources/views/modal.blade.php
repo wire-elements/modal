48c282f61200dcb63c83cd311fc2d7323c0c05f8
Harings Rob <haringsrob@gmail.com>
2022-10-06 14:43:35 +0200

    Update modal.blade.php

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -43,7 +43,7 @@
                     x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                     x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     x-bind:class="modalWidth"
-                    class="inline-block w-full align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full"
+                    class="inline-block w-full align-bottom bg-white rounded-lg text-left overflow-visible shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full"
                     id="modal-container"
             >
                 @forelse($components as $id => $component)
