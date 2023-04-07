8cd30f6868779b90f7e9cbd063d2b75d0ed6fa1d
Philo Hermans <me@philo.dev>
2023-04-07 14:32:19 +0200

    Add tests

--- tests/Components/DemoModal.php
+++ tests/Components/DemoModal.php
@@ -3,14 +3,25 @@
 namespace LivewireUI\Modal\Tests\Components;
 
 use LivewireUI\Modal\ModalComponent;
+use LivewireUI\Modal\Tests\Models\TestUser;
 
 class DemoModal extends ModalComponent
 {
+    public TestUser $user;
+    public $number;
+    public $message;
+
+    public function mount(TestUser $user)
+    {
+        $this->user = $user;
+    }
+
     public function render()
     {
-        return <<<'blade'
+        return <<<blade
             <div>
-                Hello
+                {$this->user->first_name} says:
+                {$this->message} + {$this->number}
             </div>
         blade;
     }
