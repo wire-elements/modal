d719239e95f49ae7904c08ae8018b38fd14be48d
wlhrtr <weilharterpeter@live.com>
2025-02-20 13:53:04 +0100

    Fix focusables selector in modal.js (#500)

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -121,7 +121,7 @@ window.LivewireUIModal = () => {
             });
         },
         focusables() {
-            let selector = 'a, button, input:not([type=\'hidden\'], textarea, select, details, [tabindex]:not([tabindex=\'-1\']))'
+            let selector = 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
 
             return [...this.$el.querySelectorAll(selector)]
                 .filter(el => !el.hasAttribute('disabled'))
