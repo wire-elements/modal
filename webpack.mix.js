4e955ca73f5913501d76ba05fe5f7cdaf4022946
Philo Hermans <me@philohermans.com>
2025-03-23 13:09:13 +0800

    Tailwind v4

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/modal.js', 'public/')
     .postCss("resources/css/modal.css", "public/", [
-        require("tailwindcss"),
+        require("@tailwindcss/postcss"),
     ]);
